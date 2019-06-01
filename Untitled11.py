677acd2b9cd6a0953e1437a9654e859b9d0a0e36
sai vamsi yelamanchili <44221627+wonderfulvamsi@users.noreply.github.com>
2019-06-01 20:11:06 +0530

    Add files via upload

---
+++ Untitled11.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This note book is made for my coresera capstone project..
+# ===
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
